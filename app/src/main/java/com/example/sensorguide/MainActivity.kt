5cc583fde6b96c5dc077df2d1ea437e6f2242986
shubhammogarkar7400 <shubhammogarkar7400@gmail.com>
2023-12-28 12:52:00 +0530

    first commit

---
+++ app/src/main/java/com/example/sensorguide/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.sensorguide
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.sensorguide.ui.theme.SensorGuideTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SensorGuideTheme {
+                // A surface container using the 'background' color from the theme
+                val isDark by remember {
+                    mutableStateOf(viewModel.isDark)
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            if (isDark) Color.DarkGray else Color.White
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = if(isDark) {
+                            "It's dark outside"
+                        } else {
+                            "It's bright outside"
+                        },
+                        color = if(isDark) Color.White else Color.DarkGray
+                    )
+                }
+            }
+        }
+    }
+}
+
